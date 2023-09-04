53759d8792cacab2dc46e64c4a6f79dc61a5a2b1
hetdungrani <142708650+hetdungrani@users.noreply.github.com>
2023-09-04 15:44:26 +0530

    vowels.c

--- vowels.c
+++ vowels.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 void main()
 {
-    char X, a,e,i,o,u,U,A,E,I,O;
+    char X, a,e,i,o,u;
     printf("enter a charecater ");
     scanf("%c", &X);
 
-    if (X == a || X == e || X == i || X == o || X == u)
+    if (X == 'a' || X == 'e' || X == 'i' || X == 'o' || X == 'u')
     {
         printf("vowels");
     }
-    else if(X == A|| X == E || X == I || X == O || X == U){
-         printf("conjucthions");
-    }
+  
     else{
-        printf("no");
+        printf("conjucthions");
     }
    
 }
