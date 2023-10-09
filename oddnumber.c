539277c00413c7f2e980193724831b53417fee94
hetdungrani <142708650+hetdungrani@users.noreply.github.com>
2023-10-09 18:44:09 +0530

    Add files via upload

---
+++ oddnumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("Enter a  start number:");
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d \n", i);
+        }
+    }
+}
